e95b6645091faa2624cd4ed15a52a910c152f6c6
张中俊 <1014830422@qq.com>
2018-12-14 08:58:39 +0800

    Update App2.java

--- 基本数据类型/src/main/java/com/zhong/App2.java
+++ 基本数据类型/src/main/java/com/zhong/App2.java
@@ -6,12 +6,20 @@ package com.zhong;
  */
 public class App2
 {
-    public static void main( String[] args ) throws InterruptedException {
-        float a = 0.01;
-        float b = 0.88;
+    public static void main(String[] args) throws InterruptedException {
+        float a = 0.01f;
+        float b = 0.88f;
         float c = a + b;
-            System.out.print(String.format("%.10f",c));
-        System.out.println(c<0.89);
+        System.out.println(String.format("%.2f", c));
+        System.out.println(String.format("%.3f", c));
+        System.out.println(String.format("%.4f", c));
+        System.out.println(String.format("%.5f", c));
+        System.out.println(String.format("%.6f", c));
+        System.out.println(String.format("%.7f", c));
+        System.out.println(String.format("%.8f", c));
+        System.out.println(String.format("%.9f", c));
+        System.out.println(String.format("%.10f", c));
 
+        System.out.println(c < 0.89);
     }
 }
